bf01cba1b1b6214fa4abea5b35d44735cf70816a
Nischay Ram Mamidi <NischayPro@gmail.com>
2022-05-24 12:36:12 +0530

    added img

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import Highlight from "@site/src/components/Highlight";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx("hero hero--primary home__image", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -32,6 +32,10 @@ export default function Home() {
         <Highlight
           title=""
           content="Sharanga is the high performance computing cluster at the BITS Pilani - Hyderabad Campus. This facility is meant for research purposes. It is a heterogeneous system consisting of 22 compute nodes and 4 accelerator nodes, supporting CPU and GPU parallel computations. The total computing power in terms of Rpeak is around 200 Teraflops."
+        />
+        <Highlight
+          title=""
+          content=""
         />
       </main>
     </Layout>
